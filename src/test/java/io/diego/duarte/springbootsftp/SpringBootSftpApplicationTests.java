02c496ffd7d2dd51110a3fef6b20ab3e1cca017a
Diego Duarte <diegoduarte98@gmail.com>
2021-10-05 09:48:46 -0300

    first commit

---
+++ src/test/java/io/diego/duarte/springbootsftp/SpringBootSftpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.diego.duarte.springbootsftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSftpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
